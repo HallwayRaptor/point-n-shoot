2fec9cff28e6ba5393a9f54d0b8e324ac4b3586b
Rob <hallwayraptor@gmail.com>
2022-07-30 10:12:36 -0300

    add collision

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-ctx.font = '50px Impact';
 
+const collisionCanvas = document.getElementById("collisionCanvas");
+const collisionCtx = canvas.getContext("2d");
+collisionCanvas.width = window.innerWidth;
+collisionCanvas.height = window.innerHeight;
+
+ctx.font = "50px Impact";
 let score = 0;
+
 let timeToNextRaven = 0;
 let ravenInterval = 500;
 let lastTime = 0;
@@ -13,8 +19,8 @@ let ravens = [];
 class Raven {
 	constructor() {
 		this.spriteWidth = 271;
-    this.spriteHeight = 194;
-    this.sizeModifier = Math.random() * 0.6 + 0.4;
+		this.spriteHeight = 194;
+		this.sizeModifier = Math.random() * 0.6 + 0.4;
 		this.width = this.spriteWidth * this.sizeModifier;
 		this.height = this.spriteHeight * this.sizeModifier;
 		this.x = canvas.width;
@@ -23,29 +29,43 @@ class Raven {
 		this.directionY = Math.random() * 5 - 2.5;
 		this.markedForDeletion = false;
 		this.image = new Image();
-    this.image.src = "raven.png";
-    this.frame = 0;
-    this.maxFrame = 4;
-    this.timeSinceFlap = 0;
-    this.flapInterval = Math.random() * 50 + 50;
+		this.image.src = "raven.png";
+		this.frame = 0;
+		this.maxFrame = 4;
+		this.timeSinceFlap = 0;
+		this.flapInterval = Math.random() * 50 + 50;
+		this.randomColors = [
+			Math.floor(Math.random() * 255),
+			Math.floor(Math.random() * 255),
+			Math.floor(Math.random() * 255),
+		];
+		this.color =
+			"rgb(" +
+			this.randomColors[0] +
+			"," +
+			this.randomColors[1] +
+			"," +
+			this.randomColors[2] +
+			")";
 	}
 
-  update(deltatime) {
-    if (this.y < 0 || this.y > canvas.height - this.height) {
-      this.directionY = this.directionY * -1;
-    }
-    this.x -= this.directionX;
-    this.y += this.directionY;
-    if (this.x < 0 - this.width) this.markedForDeletion = true;
-    this.timeSinceFlap += deltatime;
-    if (this.timeSinceFlap > this.flapInterval) {
-      if (this.frame > this.maxFrame) this.frame = 0;
-      else this.frame++;
-      this.timeSinceFlap = 0;
-    }
+	update(deltatime) {
+		if (this.y < 0 || this.y > canvas.height - this.height) {
+			this.directionY = this.directionY * -1;
+		}
+		this.x -= this.directionX;
+		this.y += this.directionY;
+		if (this.x < 0 - this.width) this.markedForDeletion = true;
+		this.timeSinceFlap += deltatime;
+		if (this.timeSinceFlap > this.flapInterval) {
+			if (this.frame > this.maxFrame) this.frame = 0;
+			else this.frame++;
+			this.timeSinceFlap = 0;
+		}
 	}
 	draw() {
-		ctx.strokeRect(this.x, this.y, this.width, this.height);
+		collisionCtx.fillStyle = this.color;
+		collisionCtx.fillRect(this.x, this.y, this.width, this.height);
 		ctx.drawImage(
 			this.image,
 			this.frame * this.spriteWidth,
@@ -63,28 +83,42 @@ class Raven {
 // const raven = new Raven();
 
 function drawScore() {
-  ctx.fillStyle = 'black';
-  ctx.fillText(`Score:` + score, 50, 75);
-  ctx.fillStyle = 'white';
-  ctx.fillText(`Score:` + score, 53, 78);
+	ctx.fillStyle = "black";
+	ctx.fillText(`Score:` + score, 50, 75);
+	ctx.fillStyle = "white";
+	ctx.fillText(`Score:` + score, 53, 78);
 }
 
-window.addEventListener('click', function (e) {
-  console.log(e.x, e.y);
-  const detectPixelColor = ctx.getImageData(e.x, e.y, 1, 1)
-  console.log(detectPixelColor);
-})
+window.addEventListener("click", function (e) {
+	const detectPixelColor = collisionCtx.getImageData(e.x, e.y, 1, 1);
+	console.log(detectPixelColor);
+	const pc = detectPixelColor.data;
+	ravens.forEach((object) => {
+		if (
+			object.randomColors[0] === pc[0] &&
+			object.randomColors[1] === pc[1] &&
+			object.randomColors[2] === pc[2]
+		) {
+			object.markedForDeletion = true;
+			score++;
+		}
+	});
+});
 
 function animate(timestamp) {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	collisionCtx.clearRect(0, 0, canvas.width, canvas.height);
 	let deltatime = timestamp - lastTime;
 	lastTime = timestamp;
 	timeToNextRaven += deltatime;
 	if (timeToNextRaven > ravenInterval) {
 		ravens.push(new Raven());
 		timeToNextRaven = 0;
-  }
-  drawScore();
+		ravens.sort(function (a, b) {
+			return a.width - b.width;
+		});
+	}
+	drawScore();
 	[...ravens].forEach((object) => object.update(deltatime));
 	[...ravens].forEach((object) => object.draw());
 	ravens = ravens.filter((object) => !object.markedForDeletion);
